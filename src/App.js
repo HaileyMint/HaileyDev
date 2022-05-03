09cc3e4aacb66c42ae8aaadb080e2a4f2b1aed7d
hailey <peppermint7825@gmail.com>
2022-05-03 17:44:15 +0900

    App.js edit test2
    how to operate "git" when update any files.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>테스트중입니다. That's not bad.</p>
+        <p>테스트중입니다. That's not bad.111</p>
         <a
           className="App-link"
           href="https://reactjs.org"
